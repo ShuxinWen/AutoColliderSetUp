02f04e02315952c626e68cd18331f4f9ad17f15c
ShuxinWen <1390306403@qq.com>
2021-08-26 22:34:24 +0800

    Initial commit

---
+++ Assets/FTP_AutoColliderSetUp/Editor/ColliderToolsWindow.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace FastToolsPackage.AutoWrapBodyCollider
+{
+    //
+    // Copy Line-Sphere and clear Colliders of Character
+    //
+    public class ColliderToolsWindow : EditorWindow
+    {
+        [MenuItem("FTP_Tools/FTP - ColliderTools", false, 2001)]
+        public static void DoWindow()
+        {
+            var window = GetWindow<ColliderToolsWindow>("FTP Collider Tools");
+            window.minSize = new Vector2(200, 300);
+            window.Show();
+        }
+
+        public GameObject m_Src;
+        public GameObject m_Des;
+
+        public GameObject m_ClearHumanoidColliderTarget;
+        private HumanBodyBoneReferenceData newAvatarBoneData;
+
+
+        private void OnGUI()
+        {
+            EditorGUILayout.HelpBox("Copy , Clear Humanoid Collider", MessageType.Info);
+            EditorTools.DrawLabelWithColorInBox("Copy", Color.green);
+
+            m_Src = EditorGUILayout.ObjectField("Source", m_Src, typeof(GameObject), true) as GameObject;
+            m_Des = EditorGUILayout.ObjectField("Destination", m_Des, typeof(GameObject), true) as GameObject;
+
+            // If your humanoid character has same "bone" name
+            if (GUILayout.Button("Copy Line-Sphere With Same Hierarchy", GUILayout.MinHeight(25)))
+            {
+                if (m_Src == null || m_Des == null)
+                {
+                    EditorTools.ShowMessage("Src or Des is Null...");
+                }
+                else
+                {
+                    FTPColliderTools.CopyLineSphereColliderWithSameHierarchy(m_Src, m_Des);
+                }
+            }
+            // If your humanoid character has unsame "bone" name you can use Avatar to copy Collider
+            if (GUILayout.Button("Copy Line-Sphere with Avatar Settings", GUILayout.MinHeight(25)))
+            {
+                if (m_Src == null || m_Des == null)
+                {
+                    EditorTools.ShowMessage("Src or Des is Null...");
+                }
+                else
+                {
+                    FTPColliderTools.CopyLineSphereColliderWithAvatar(m_Src, m_Des);
+                }
+            }
+
+            EditorTools.DrawSpace(4);
+            EditorTools.DrawLabelWithColorInBox("Clear", Color.green);
+            m_ClearHumanoidColliderTarget = EditorGUILayout.ObjectField("Clear Target", m_ClearHumanoidColliderTarget, typeof(GameObject), true) as GameObject;
+            // clear line-sphere collider
+            if (GUILayout.Button("Clear Line-Sphere Collider", GUILayout.MinHeight(25)))
+            {
+                FTPColliderTools.ClearLineSphereCollider(m_ClearHumanoidColliderTarget);
+            }
+
+            // clear normal box or other real collider
+            // just clear bone collider with avatar bone map
+            if (GUILayout.Button("Clear Normal Collider", GUILayout.MinHeight(25)))
+            {
+                if (m_ClearHumanoidColliderTarget != null)
+                {
+                    Animator animator = m_ClearHumanoidColliderTarget.GetComponent<Animator>();
+                    if (animator == null || !animator.isHuman || animator.avatar == null)
+                    {
+                        EditorTools.ShowMessage("Collider Target Need Animator to get avatar bone map.check Collider target is the Animator root?" +
+                            "And We need Humanoid Target and with Avatar set");
+                    }
+                    else
+                    {
+                        if (EditorUtility.DisplayDialog("Message", "Clear Humanoid Normal Collider with Avatar Bone?", "OK", "Cancel"))
+                        {
+                            if (newAvatarBoneData == null)
+                                newAvatarBoneData = new HumanBodyBoneReferenceData();
+                            newAvatarBoneData.ResetReference();
+                            newAvatarBoneData.MapHumanAvatarToBoneReferences(m_ClearHumanoidColliderTarget.transform, animator);
+
+                            foreach (var bone in newAvatarBoneData._dicBones)
+                            {
+                                if (bone.Value != null)
+                                {
+                                    Collider[] colliders = bone.Value.GetComponents<Collider>();
+                                    for (int i = 0; i < colliders.Length; i++) { Object.DestroyImmediate(colliders[i]); }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/FTP_AutoColliderSetUp/Editor/FTPColliderTools.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace FastToolsPackage.AutoWrapBodyCollider
+{
+    public class FTPColliderTools
+    {
+        public static void CopyLineSphereColliderWithAvatar(GameObject src, GameObject des)
+        {
+            if (src == null || des == null)
+            {
+                Debug.LogError("_src == null || _des == null");
+                return;
+            }
+
+            Animator animatorSrc = src.GetComponent<Animator>();
+            Animator animatorDes = des.GetComponent<Animator>();
+            if (animatorSrc == null || animatorDes == null)
+            {
+                Debug.LogError("animator src == null || animator src == null");
+                return;
+            }
+
+            int boneCount = (int)HumanBodyBones.LastBone - 1;
+            for (int i = 0; i < boneCount; i++)
+            {
+                HumanBodyBones bone = (HumanBodyBones)i;
+                Transform trs01 = animatorSrc.GetBoneTransform(bone);
+                Transform trsDes = animatorDes.GetBoneTransform(bone);
+                if (trs01 == null)
+                    continue;
+                if (trs01 != null && trsDes == null)
+                {
+                    Debug.LogError("Two humanoid does have the same bone please Check");
+                    return;
+                }
+
+                LineSphereCollider[] colliders = trs01.GetComponents<LineSphereCollider>();
+
+                RootColliderManager smanager = trsDes.GetComponent<RootColliderManager>();
+                if (smanager == null)
+                    smanager = trsDes.gameObject.AddComponent<RootColliderManager>();
+
+                for (int index = 0; index < colliders.Length; index++)
+                {
+                    LineSphereCollider sp = trsDes.gameObject.AddComponent<LineSphereCollider>();
+                    LineSphereCollider s1 = colliders[index];
+
+                    sp.A = s1.A;
+                    sp.B = s1.B;
+                    sp.RadiusA = s1.RadiusA;
+                    sp.RadiusB = s1.RadiusB;
+
+                    sp.scaleAFactor = s1.scaleAFactor;
+                    sp.scaleBFactor = s1.scaleBFactor;
+
+                    sp.EditorAFactor = s1.EditorAFactor;
+                    sp.EditorBFactor = s1.EditorBFactor;
+                    sp.lineIndex = s1.lineIndex;
+                    sp._enableCollider = s1._enableCollider;
+                    sp._startBone = s1._startBone;
+                    sp._endBone = s1._endBone;
+
+                    smanager._lineSphereColliders.Add(sp);
+                }
+            }
+        }
+        public static void CopyLineSphereColliderWithSameHierarchy(GameObject src, GameObject des)
+        {
+            if (src == null || des == null)
+            {
+                Debug.LogError("_src == null || _des == null");
+                return;
+            }
+            else
+            {
+                LineSphereCollider[] scripts = src.GetComponentsInChildren<LineSphereCollider>(true);
+                for (int i = 0; i < scripts.Length; i++)
+                {
+                    Transform trs = scripts[i].transform;
+                    string path = AnimationUtility.CalculateTransformPath(trs, src.transform);
+
+                    Transform desTrs = des.transform.Find(path);
+                    if (desTrs == null)
+                    {
+                        Debug.LogError("Fail: src and des dismatch in Hierarchy...");
+                        return;
+                    }
+                    RootColliderManager smanager = desTrs.GetComponent<RootColliderManager>();
+                    if (smanager == null)
+                        smanager = desTrs.gameObject.AddComponent<RootColliderManager>();
+
+                    LineSphereCollider sp = desTrs.gameObject.AddComponent<LineSphereCollider>();
+                    LineSphereCollider s1 = scripts[i];
+
+                    sp.A = s1.A;
+                    sp.B = s1.B;
+                    sp.RadiusA = s1.RadiusA;
+                    sp.RadiusB = s1.RadiusB;
+
+                    sp.scaleAFactor = s1.scaleAFactor;
+                    sp.scaleBFactor = s1.scaleBFactor;
+
+                    sp.EditorAFactor = s1.EditorAFactor;
+                    sp.EditorBFactor = s1.EditorBFactor;
+                    sp.lineIndex = s1.lineIndex;
+                    sp._enableCollider = s1._enableCollider;
+                    sp._startBone = s1._startBone;
+                    sp._endBone = s1._endBone;
+
+                    smanager._lineSphereColliders.Add(sp);
+                }
+            }
+        }
+
+        public static void ClearLineSphereCollider(GameObject clearTarget)
+        {
+            if (clearTarget != null)
+            {
+                LineSphereCollider[] scripts = clearTarget.GetComponentsInChildren<LineSphereCollider>(true);
+                RootColliderManager[] managers = clearTarget.GetComponentsInChildren<RootColliderManager>(true);
+                for (int i = 0; i < scripts.Length; i++)
+                {
+                    GameObject.DestroyImmediate(scripts[i]);
+                }
+
+                for (int i = 0; i < managers.Length; i++)
+                {
+                    GameObject.DestroyImmediate(managers[i]);
+                }
+            }
+        }
+
+        public static void ClearNormalCollider(GameObject clearTarget)
+        {
+
+        }
+    }
+}
